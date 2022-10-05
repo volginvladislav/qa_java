45efd976474200a44352ac755c3dfa7b7896431d
volginvladislav <volgingit@gmail.com>
2022-10-05 13:49:27 +0300

    исправил латиницу и добавил тест на отработку эксепшн при невалидном значении аргумента в методе getFood

--- src/test/java/com/example/AnimalTest.java
+++ src/test/java/com/example/AnimalTest.java
@@ -7,15 +7,19 @@ import static org.junit.Assert.*;
 public class AnimalTest {
 
     @Test
-    public void getFamilyShouldReturnСorrectText() {
+    public void getFamilyShouldReturnCorrectText() {
         Animal animal = new Animal();
         String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
         assertEquals(expected, animal.getFamily());
     }
+    @Test(expected = Exception.class)
+    public void getFoodInvalidAnimalKindShouldReturnExepton() throws Exception {
+        new Animal().getFood("Всеядное");
+    }
     @Test
     public void catchExceptionWhenFelineHasInvalidAnimalKind() {
         Animal animal = new Animal();
-        Exception exception = assertThrows(Exception.class, () -> animal.getFood("Падальщик"));
+        Exception exception = assertThrows(Exception.class, () -> animal.getFood("Всеядное"));
         assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", exception.getMessage());
     }
 }
